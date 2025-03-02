fdf14bb3a7a4f9d1fe50b6e95b6963b554e5724e
farhanalishah23 <farhan.alishah12@gmail.com>
2025-03-02 21:07:50 +0500

    newfiles added

--- createapi.php
+++ createapi.php
@@ -1,6 +1,22 @@
-<?php 
+<?php
 include "connection.php";
 
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers:Access-Control-Allow-Headers ,
+Content-Type,Access-Control-Allow-Methods, 
+Authorization , X-Requested-With');
 
+$data = json_decode(file_get_contents("php://input"), true);
+$user_name = $data["sname"];
+$user_email = $data["semail"];
+$user_description = $data["sdescription"];
 
-?>
+$sql = "INSERT INTO user (name,email,description) VALUES ('{$user_name}','{$user_email}','{$user_description}')";
+
+if (mysqli_query($conn, $sql)) {
+    echo json_encode(array("message" => "User Data Inserted Successfully.", "status", true));
+} else {
+    echo json_encode(array("message" => "You Are Failed To Insert User Data.", "status", true));
+}
