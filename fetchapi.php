91ed83e0340ae4d3c27f06a8b21006cba6798011
farhanalishah23 <farhan.alishah12@gmail.com>
2025-03-02 18:26:14 +0500

    newchanges

---
+++ fetchapi.php
@@ -0,0 +1,17 @@
+<?php 
+include "connection.php";
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+$sql = 'SELECT * FROM user';
+$result =mysqli_query($conn,$sql) or die("SQL QUERY FAILED.");
+
+if(mysqli_num_rows($result) > 0){
+    $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    echo json_encode($output);
+}else{
+    echo json_encode(array("message"=>"Data not found","status"=>false));
+}
+
+?>
---
+++ singlefetch.php
@@ -0,0 +1,22 @@
+<?php
+include "connection.php";
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+$data = json_decode(file_get_contents("php://input"), true);  //Koi data Arraha han ossy Fetch krna use hote han file_gets_content like form se data araha ho api me
+$user_id = $data['uid'];
+
+$sql = "SELECT * FROM user WHERE id = {$user_id}";
+
+$result = mysqli_query($conn, $sql) or ("SQL QUERY FAILED.");
+
+if (mysqli_num_rows($result) > 0) {
+    $output = mysqli_fetch_all($result);
+       echo json_encode($output);
+}else{
+   echo json_encode(array("message"=>"Data not found","status",false));
+}
+
+
+?>
