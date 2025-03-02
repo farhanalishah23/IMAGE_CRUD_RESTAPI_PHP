a47d9e52bd67335931ab5f6983a9861edc2bb1a3
farhanalishah23 <farhan.alishah12@gmail.com>
2025-03-03 01:18:33 +0500

    newc

--- deleteapi.php
+++ deleteapi.php
@@ -1,3 +1,21 @@
 <?php
+include "connection.php";
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type , Access-Control-Allow-Methods, Authorization,X-Requested-With');
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$id = $data["uid"];
+
+$sql = "DELETE FROM user WHERE id = '{$id}'";
+
+if (mysqli_query($conn, $sql)) {
+    echo json_encode(array("Message" => "User Data Deleted Successfully.", "status" => true));
+} else {
+    echo json_encode(array("Message" => "User Data not found.", "status" => false));
+}
 
 ?>
