913d70302c309863a2eeb5e4d62ce5751d04d575
farhanalishah23 <farhan.alishah12@gmail.com>
2025-03-04 22:35:46 +0500

    newchange

--- createapi.php
+++ createapi.php
@@ -8,10 +8,9 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers ,Content-Type
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-$user_name = $data["uname"];
-$user_email = $data["uemail"];
-$user_description = $data["udescription"];
-$user_image = $data["image"];
+$user_name = $data["uname"] ? $data["uname"] :"no user name";
+$user_email = $data["uemail"] ? $data["uemail"] : "no user email";
+$user_description = $data["udescription"] ? $data["udescription"] : "no user description";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['image'])) {
     $fileName = $_FILES['image']['name'];
@@ -26,12 +25,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['image'])) {
 
     if (in_array($fileExtention, $allowedExtention)) {
 
-        $FileUniqueName = uniqid('',true) . '.'. $fileExtention;
+        $FileUniqueName = uniqid('', true) . '.' . $fileExtention;
         $fileDirectory = 'uploads/';
-        
+
+        if (!is_dir($fileDirectory)) {
+            mkdir($fileDirectory, 0777, true);
+        }
+
+        if ($fileSize > 5 * 1024 * 1024) {
+            echo json_encode(array("message" => "FileSize is not allowed more than 5MB.", "status" => true));
+        }
+
         $targetFileUpload = $fileDirectory . $FileUniqueName;
 
-        if(move_uploaded_file($tmpName,$targetFileUpload)){
+        if (move_uploaded_file($tmpName, $targetFileUpload)) {
             $sql = "INSERT INTO user (name,email,description,image) VALUES ('{$user_name}','{$user_email}','{$user_description}','{$targetFileUpload}')";
 
             if (mysqli_query($conn, $sql)) {
@@ -39,10 +46,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['image'])) {
             } else {
                 echo json_encode(array("message" => "You Are Failed To Insert User Data.", "status", false));
             }
-        }else{
+        } else {
             echo json_encode(array("message" => "You Are Failed Upload User Image in directory.", "status", false));
         }
-     
+    } else {
+        echo json_encode(array("message" => "Only jpg,jpeg,png and gif extentions are allowed to upload", "status" => false));
     }
 } else {
     echo json_encode(array("message" => "You Are Failed To Upload User Image.", "status", false));
